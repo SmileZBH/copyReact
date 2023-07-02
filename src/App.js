6cd35a3f1e41243469abcf0855c29f48c64acbda
SmileZBH <1494076387@qq.com>
2023-07-02 22:47:19 +0800

    最新

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from 'react'
 // components
-import Banner from './components/Banner';
-import Header from './components/Header';
-import Nav from './components/Nav';
-import About from './components/About';
-import Services from './components/Services';
-import Work from './components/Work';
-import Contact from './components/Contact';
+import Banner from './components/Banner'
+import Header from './components/Header'
+import Nav from './components/Nav'
+import About from './components/About'
+import Services from './components/Services'
+import Work from './components/Work'
+import Contact from './components/Contact'
 
 const App = () => {
   return (
@@ -18,9 +18,9 @@ const App = () => {
       <Services />
       <Work />
       <Contact />
-      {/* <div className='h-[4000px]'></div> */}
+      <div className='h-[4000px]'></div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
